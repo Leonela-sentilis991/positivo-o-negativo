6e356bb4b39be500e781a54e0e6cc230c3476d78
Leonela-sentilis991 <leonelasentilis0@gmail.com>
2024-03-03 18:55:29 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("positio o negativo");
+
+Console.Write("ingrese un numero:   ");
+int numero = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine();
+
+if (numero > 0)
+{
+    Console.WriteLine("el numero es positivo");
+}
+else
+{
+    Console.WriteLine("el numero es negativo");
+}
